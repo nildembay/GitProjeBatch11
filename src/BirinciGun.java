01a945fafa5a0fd7623547745bee5e61c01a4369
nildembay <nildembay@gmail.com>
2020-11-17 00:01:27 +0300

    ikinci commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -2,7 +2,8 @@
 public class BirinciGun {
 	
 	public static void main(String[] args) {
-		System.out.println("11:05 birinci gün");
+		System.out.println("11:57 birinci gün");
+		System.out.println("ikinci commit");
 	}
 
 }
