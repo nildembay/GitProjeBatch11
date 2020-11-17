7416f52c17bc8cc07da6215a74b4d653eb2986a3
nildembay <74560545+nildembay@users.noreply.github.com>
2020-11-17 22:14:29 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -2,8 +2,9 @@
 public class BirinciGun {
 	
 	public static void main(String[] args) {
-		System.out.println("11:57 birinci g�n");
+		System.out.println("11:57 birinci gün");
 		System.out.println("ikinci commit");
+		System.out.println("10:11 ikinci gün");
 	}
 
 }
