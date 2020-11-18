e7ae776b7d58ec234b14b28f3f4d13f79415603b
nildembay <nildembay@gmail.com>
2020-11-18 23:06:42 +0300

    mesaj

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -13,7 +13,7 @@ public class BirinciGun {
 		System.out.println("yepyeni bir satir");
 		System.out.println("10:26 feature branch");
 		System.out.println("10:50 feature branch");
-
+		System.out.println("master 11:05");
 
 
 	}
